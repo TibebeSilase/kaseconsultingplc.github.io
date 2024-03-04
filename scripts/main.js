88d960d7e798c40c77609efdd6a9c808ce3b9446
TibebeSilase <136748611+TibebeSilase@users.noreply.github.com>
2024-03-04 22:41:04 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// let servicesMenu = document.querySelector('#services-menu');
+
+// let servicesSubMenu = document.querySelector('.sub-menu');
+
+// servicesMenu.addEventListener('click', () => {
+
+//     servicesSubMenu.style.display = 'block';
+
+// })
+
